ced8c7c1e1ee8b099fb6b2a58889997d8e2c4b85
Alex <ale77.salazar@gmail.com>
2019-06-03 21:26:42 -0300

    Ahora para crear el popup se usa bootstrap

--- vista/contenidos/sectores.php
+++ vista/contenidos/sectores.php
@@ -1,36 +1,46 @@
+<!-- Botones con ID para el control del click -->
 
 <div class="row">
 	<div class="col-md-6">
-		<button id="buffet" class="btn btn-info btn-block boton-sector"><i class="fas fa-pizza-slice"></i> Buffet</button>
+		<button type="button" id="buffet" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-pizza-slice"></i> Buffet</button>
 	</div>
 	<div class="col-md-6">
-		<button id="sala" class="btn btn-info btn-block boton-sector"><i class="fas fa-book"></i> Sala de estudio</button>
+		<button type="button" id="sala" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-book"></i> Sala de estudio</button>
 	</div>
 	<div class="col-md-6">
-		<button id="fotocopiadora" class="btn btn-info btn-block boton-sector"><i class="fas fa-hdd"></i> Fotocopiadoras</button>
+		<button type="button" id="fotocopiadora" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-hdd"></i> Fotocopiadoras</button>
 	</div>
 	<div class="col-md-6">
-		<button id="departamentos" class="btn btn-info btn-block boton-sector"><i class="fas fa-city"></i> Departamentos</button>
+		<button type="button" id="departamentos" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-city"></i> Departamentos</button>
 	</div>
 	<div class="col-md-6">
-		<button id="laboratorio" class="btn btn-info btn-block boton-sector"><i class="fas fa-flask"></i> Laboratorio</button>
+		<button type="button" id="laboratorio" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-flask"></i> Laboratorio</button>
 	</div>
 	<div class="col-md-6">
-		<button id="baños" class="btn btn-info btn-block boton-sector"><i class="fas fa-toilet"></i> Baños</button>
+		<button type="button" id="baños" class="btn btn-info btn-block boton-sector" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-toilet"></i> Baños</button>
 	</div>
 </div>
 
 
-
-<div class="overlay" id="overlay">
-	<div class="popup" id="popup">
-		<a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
+<!-- El popup hecho con bootstrap -->
+<div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Mapa de la ubicación</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      	<!-- La imagen que se muestra en pantalla se escoge segun el boton que se presione "popup.js" -->
 		<img class="imagen-sector" id="selector-imagen">
-		<h2 class="lead">Es posible agregarle texto</h2>
-	</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
 </div>
-
-
 
 <script type="text/javascript" src="vista/js/popup.js"></script>
-
--- vista/js/popup.js
+++ vista/js/popup.js
@@ -1,3 +1,4 @@
+
 // Importo las ID de los elementos a usar
 
 var abrirBuffet = document.getElementById('buffet'),
@@ -7,57 +8,32 @@ var abrirBuffet = document.getElementById('buffet'),
 	abrirLaboratorio = document.getElementById('laboratorio'),
 	abrirBaños = document.getElementById('baños'),
 
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	cerrar = document.getElementById('btn-cerrar-popup'),
 	imagen = document.getElementById('selector-imagen');
 
 
-// agrego imagen segun donde se haya hecho un click
+// Se agrega una imagen segun donde se haya hecho un click
 
 abrirBuffet.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/buffet.jpg');
 })
 
 abrirSala.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/sala.jpg');
 })
 
 abrirFotocopiadora.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/foto.jpg');
 })
 
 abrirDepartamentos.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/3.jpg');
 })
 
 abrirLaboratorio.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/3.jpg');
 })
 
 abrirBaños.addEventListener('click', function(){
-	abrirVentana();
 	imagen.setAttribute('src', 'vista/img/3.jpg');
 })
 
-
-
-// Boton de cerrar de la ventana POPUP
-
-cerrar.addEventListener('click', function(){
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-})
-
-
-// Funcion que abre la ventana
-
-function abrirVentana(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-}
