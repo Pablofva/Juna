799b05dcf5f353cffef375aa3d923d6a2d507738
Pablo <pablovzqz768@gmail.com>
2019-07-01 16:17:54 -0300

    eliminie

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "core/configGeneral.php";
-
-    require_once "controlador/vistaControlador.php";
-
-    $plantilla = new vistaControlador();
-    $plantilla->obtener_plantilla_controlador();
-?>
