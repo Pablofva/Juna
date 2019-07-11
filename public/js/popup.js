32847a6dcf27454b600adaf19eca7024ca5328ef
Alex <ale77_salazar@hotmail.com.ar>
2019-07-11 13:27:18 -0300

    nueva version laravel

---
+++ public/js/popup.js
@@ -0,0 +1,39 @@
+//alert("Alex");
+// Importo las ID de los elementos a usar
+
+var abrirBuffet = document.getElementById('buffet'),
+	abrirSala = document.getElementById('sala'),
+	abrirFotocopiadora = document.getElementById('fotocopiadora'),
+	abrirDepartamentos = document.getElementById('departamentos'),
+	abrirLaboratorio = document.getElementById('laboratorio'),
+	abrirBaños = document.getElementById('baños'),
+
+	imagen = document.getElementById('selector-imagen');
+
+
+// Se agrega una imagen segun donde se haya hecho un click
+
+abrirBuffet.addEventListener('click', function(){
+	imagen.setAttribute('src', 'img/buffet.jpg');
+})
+
+abrirSala.addEventListener('click', function(){
+	imagen.setAttribute('src', 'vista/img/sala.jpg');
+})
+
+abrirFotocopiadora.addEventListener('click', function(){
+	imagen.setAttribute('src', 'vista/img/foto.jpg');
+})
+
+abrirDepartamentos.addEventListener('click', function(){
+	imagen.setAttribute('src', 'vista/img/3.jpg');
+})
+
+abrirLaboratorio.addEventListener('click', function(){
+	imagen.setAttribute('src', 'vista/img/3.jpg');
+})
+
+abrirBaños.addEventListener('click', function(){
+	imagen.setAttribute('src', 'vista/img/3.jpg');
+})
+
---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,29 @@
+@extends('plantilla')
+
+@section('content')
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <h1>Iniciar sesión como administrador</h1>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+@endsection
