9fb1a27e0d6417f284ac637581dfeb3a3443ee71
Alex <ale77_salazar@hotmail.com.ar>
2019-07-11 21:21:05 -0300

    agregado pagina de institutos

--- resources/views/aulas.blade.php
+++ resources/views/aulas.blade.php
@@ -2,27 +2,29 @@
 
 @section('content')
     
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1>Aqui el buscador y los filtros para buscar las aulas</h1>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+<h1 class="letra-titulo pt-5 pb-4"><strong>Seleccione el instituto</strong></h1>
+
+<div class="row">
+    <div class="col-md-6 py-1 ">
+        <a href="" class="btn btn-ingenieria botones-inicio">
+            Ingeniería y Agronomía
+        </a>
+    </div>
+    <div class="col-md-6 py-1">
+        <a href="" class="btn btn-salud botones-inicio">
+            Ciencias de la Salud
+        </a>
+    </div>
+    <div class="col-md-6 py-1">
+        <a href="" class="btn btn-sociales botones-inicio">
+            Ciencias Sociales y Administración
+        </a>
+    </div>
+    <div class="col-md-6 py-1">
+        <a href="" class="btn btn-iniciales botones-inicio">
+            Estudios Iniciales
+        </a>
+     </div>
+</div>
 
 @endsection
